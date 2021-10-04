5fd0105e2dad7942c8fb292394306a8567bfc547
Chris <mr.chrisnash@gmail.com>
2021-10-04 16:39:54 -0700

    setting up routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './styles/App.css';
 import Nav from './components/Nav';
-import About from './components/About';
-import Shop from './components/Shop';
+import Products from './components/Products';
+import Cart from './components/Cart';
+import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <About />
-      <Shop />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/products' exact component={Products} />
+          <Route path='/cart' component={Cart} />
+          <Route path='/products/:id' component={ProductDetail}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
