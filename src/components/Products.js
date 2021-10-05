449058baca746c07af5c2bc248a897c9450c654b
Chris <mr.chrisnash@gmail.com>
2021-10-05 12:03:43 -0700

    changed the products that are displayed

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import * as ReactBootstrap from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
+import '../styles/Products.css'
 
 function Products() {
   useEffect(() => {
@@ -14,7 +16,7 @@ function Products() {
     const products=[]
 
     try{
-      for (let i = 1; i <= 3; i++) {
+      for (let i = 15; i <= 20; i++) {
         const data = await fetch(`https://fakestoreapi.com/products/${i}`);
         const items = await data.json();
         console.log(items);
@@ -28,17 +30,35 @@ function Products() {
   }
 
   return (
-    <div className="Products">
-      <h1>Products</h1>
+    <div className="products">
+      <h1 className='products-title'>Products</h1>
 
+      <div className='product-grid'>
       {loading ? 
         items.map(item => (
-          <h4 key={item.id}>
-            <Link to={`products/${item.id}`}>{item.title}</Link>
-          </h4>
+            <Card 
+              key={item.id}
+              className='product-card' 
+              style={{ width: '12rem' }}
+              border="success" 
+            >
+              <Card.Img variant="top" src={item.image} />
+              <Card.Body>
+                <Link className='product-header' to={`products/${item.id}`}>
+                  <Card.Title>{item.title}</Card.Title>
+                </Link>
+                <Card.Text className='card-text'>${item.price}</Card.Text>
+              </Card.Body>
+                <Button variant="primary">Add to cart</Button>
+            </Card>
         ))
-      : <ReactBootstrap.Spinner animation='border' variant="success" />}
-
+        : 
+          <div>
+            <h1>Loading...</h1>
+            <ReactBootstrap.Spinner animation='border' variant="success" />
+          </div>
+      }
+      </div>
     </div>
   );
 }
